8d179e4a290fedb9d197b14899d2d18c674fe584
Alexis Delgado <alexisndelgado@gmail.com>
2022-01-27 16:52:03 -0300

    chore: get value by key

--- configmaps.go
+++ configmaps.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"reflect"
 )
 
 type ConfigMap struct {
-	APIVersion string `yaml:"apiVersion"`
-	Kind       string `yaml:"kind"`
-	Metadata   struct {
+	Metadata struct {
 		Name string `yaml:"name"`
 	} `yaml:"metadata"`
 	Data interface{} `yaml:"data"`
@@ -30,3 +30,17 @@ func (cm *ConfigMap) getConfigMap(filePath string) *ConfigMap {
 func (cm *ConfigMap) getName() string { return cm.Metadata.Name }
 
 func (cm *ConfigMap) getData() interface{} { return cm.Data }
+
+func (cm *ConfigMap) getValue(requestedKey string) string {
+	data := cm.getData()
+	dict := reflect.ValueOf(data)
+	if dict.Kind() == reflect.Map {
+		for _, key := range dict.MapKeys() {
+			if key.Interface() == requestedKey {
+				value := dict.MapIndex(key).Interface()
+				return fmt.Sprint(value)
+			}
+		}
+	}
+	return ""
+}
