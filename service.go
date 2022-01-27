d52c2595a1bc1a7d38e4a5e8a651ddaad7410527
Alexis Delgado <alexisndelgado@gmail.com>
2022-01-27 18:03:42 -0300

    chore: add env vars from configmaps

--- service.go
+++ service.go
@@ -43,18 +43,33 @@ func (service *Service) getService(filePath string) *Service {
 	return service
 }
 
-func (service Service) getEnvVars(filePath string) string {
-	service.getService(filePath)
-	// TODO: identificar cronjobs que no tienen containers
+func (service Service) getEnvVarFromYaml() string {
 	containers := service.Spec.Template.Spec.Containers[0]
 	env := containers.Env
 	var str strings.Builder
 	str.WriteString("{\n")
+	var cm ConfigMap
 	for i := 0; i < len(env); i++ {
+		configmap := env[i].ValueFrom.ConfigMapKeyRef
+		if (env[i].ValueFrom.ConfigMapKeyRef) == configmap {
+			if configmap.Name != "" && configmap.Key != "" {
+				key := cm.getValue(configmap.Key)
+				str.WriteString("\t\"" + configmap.Name + "\": \"" + key + "\"\n")
+			}
+		}
 		if env[i].Value != "" {
 			str.WriteString("\t\"" + env[i].Name + "\": \"" + env[i].Value + "\"\n")
 		}
+
 	}
 	str.WriteString("}")
 	return str.String()
+}
+
+func (service Service) getEnvVars(filePath string) string {
+	service.getService(filePath)
+	// TODO: identificar cronjobs que no tienen containers
+	secrets := service.getEnvVarFromYaml()
+
+	return secrets
 }
