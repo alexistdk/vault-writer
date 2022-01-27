52cfd623cb3f30673bd9a231abb90f0b628858a7
Alexis Delgado <alexisndelgado@gmail.com>
2022-01-27 12:10:17 -0300

    feat: reads all the files in specific path

--- yaml.go
+++ yaml.go
@@ -5,6 +5,7 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 )
 
 type Yaml struct {
@@ -15,6 +16,7 @@ type Yaml struct {
 			Name     interface{} `yaml:"name"`
 			Services []struct {
 				File interface{} `yaml:"file"`
+				Path interface{} `yaml:"path"`
 			} `yaml:"services"`
 		} `yaml:"namespaces"`
 	} `yaml:"clusters"`
@@ -37,26 +39,40 @@ func (yml *Yaml) getClusters() int { return len(yml.Clusters) }
 
 func (yml *Yaml) getNamespaces(i int) int { return len(yml.Clusters[i].Namespaces) }
 
+func (yml *Yaml) getFiles(i int, j int, pathsSlice *[]string) {
+	keys := yml.Clusters[i].Namespaces[j].Services
+	for k := 0; k < len(keys); k++ {
+		if keys[k].File != nil {
+			auxPath := fmt.Sprint(keys[k].File)
+			*pathsSlice = append(*pathsSlice, auxPath)
+		} else {
+			auxPath := fmt.Sprint(keys[k].Path)
+			getFilesInPath(auxPath, pathsSlice)
+		}
+	}
+}
+
+func getFilesInPath(path string, pathsSlice *[]string) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		pathFile := filepath.Join(path, f.Name())
+		*pathsSlice = append(*pathsSlice, pathFile)
+	}
+
+}
+
 func (yml Yaml) getPaths(file string) []string {
 	yml.readYaml(file)
 	clusters := len(yml.Clusters)
-	paths := make([]string, 0, 100)
+	paths := make([]string, 0, 200)
 	for i := 0; i < clusters; i++ {
 		ns := len(yml.Clusters[i].Namespaces)
 		for j := 0; j < ns; j++ {
-			files := yml.Clusters[i].Namespaces[j].Services
-			for k := 0; k < len(files); k++ {
-				auxPath := fmt.Sprint(files[k].File)
-				paths = append(paths, auxPath)
-			}
+			yml.getFiles(i, j, &paths)
 		}
 	}
-	/*
-		files := yml.Clusters[0].Namespaces[0].Services
-		paths := make([]string, len(files))
-		for i := 0; i < len(files); i++ {
-			paths[i] = fmt.Sprint(files[i].File)
-		}
-	*/
 	return paths
 }
