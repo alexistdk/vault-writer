c67de1fba639d5ac60c23dbdb53c3c6ca5c5bc4e
Alexis Delgado <alexisndelgado@gmail.com>
2022-01-28 11:28:50 -0300

    chore: filter yamls by ns and cluster

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -14,12 +15,31 @@ func main() {
 		log.Fatal("Tenes que pasarle un yaml")
 	}
 
-	paths, configMaps := yml.getYamls(os.Args[1])
-	cmValues := cm.getValues(configMaps)
-	sliceOfScrets := make([]string, 0, 200)
-	for i := 0; i < len(paths); i++ {
-		// los secrets son strings
-		secrets := service.getEnvVars(paths[i], cmValues)
-		sliceOfScrets = append(sliceOfScrets, secrets)
+	yml.readYaml(os.Args[1])
+	clusters := len(yml.Clusters)
+	paths := make([]string, 0, 1000)
+
+	for i := 0; i < clusters; i++ {
+		ns := len(yml.Clusters[i].Namespaces)
+		for j := 0; j < ns; j++ {
+			namespaces := yml.Clusters[i].Namespaces[j].Name
+			pathsConfigMaps := make([]string, 0, 10)
+			yml.getFiles(i, j, &paths)
+			yml.getConfigMaps(i, &pathsConfigMaps)
+			cmValues := cm.getValues(pathsConfigMaps)
+			sliceOfScrets := make([]string, 0, 200)
+			for k := 0; k < len(paths); k++ {
+				// los secrets son strings
+				secrets := service.getEnvVars(paths[k], cmValues)
+				sliceOfScrets = append(sliceOfScrets, secrets)
+				fmt.Println(namespaces)
+				/*
+					fmt.Println(paths[k])
+					fmt.Println(secrets)
+					fmt.Println("---------------------------------------------------------------")
+				*/
+			}
+		}
 	}
+
 }
